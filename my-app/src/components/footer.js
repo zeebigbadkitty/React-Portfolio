8124c5821af8f1ba6431f89c7e26ff343efda640
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-13 10:58:42 -0600

    I've started to style my footer with react.

--- my-app/src/components/footer.js
+++ my-app/src/components/footer.js
@@ -1,86 +1,83 @@
-import React from 'react';
-import {
-  MDBFooter,
-  MDBContainer,
-  MDBCol,
-  MDBRow,
-} from 'mdb-react-ui-kit';
+import React from "react";
+import {ResponsiveEmbed, Image} from 'react-bootstrap';
+import { MDBFooter, MDBContainer, MDBCol, MDBRow, } from "mdb-react-ui-kit";
 
 export default function App() {
   return (
-    <MDBFooter bgColor='primary' className='text-white text-center text-lg-left'>
-      <MDBContainer className='p-4'>
+    <MDBFooter
+      className="text-center text-lg-start text-white"
+      style={{ backgroundColor: "#111155" }}
+    >
+      <MDBContainer className="p-4">
         <MDBRow>
-          <MDBCol lg='6' md='12' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'>Footer Content</h5>
+          <MDBCol lg="3" md="12" className="mb-4 mb-md-0">
+            <h5 className="text-uppercase">Candice Radam</h5>
 
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis molestias.
-              Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam voluptatem veniam, est
-              atque cumque eum delectus sint!
+              <Image className="rounded-circle shadow-4-strong"
+                source={require('../images/photo3.jpg')}  
+              />
             </p>
           </MDBCol>
 
-          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase'>Links</h5>
+          <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+            <h5 className="text-uppercase">Links</h5>
 
-            <ul className='list-unstyled mb-0'>
+            <ul className="list-unstyled mb-0">
               <li>
-                <a href='#!' className='text-white'>
-                  Link 1
-                </a>
+                <a href="#sec0">About Me</a>
               </li>
               <li>
-                <a href='#!' className='text-white'>
-                  Link 2
-                </a>
+                <a href="#sec1">Projects and Portfolio</a>
               </li>
               <li>
-                <a href='#!' className='text-white'>
-                  Link 3
-                </a>
+                <a href="#sec2">Skills</a>
               </li>
               <li>
-                <a href='#!' className='text-white'>
-                  Link 4
-                </a>
+                <a href="#sec3">Resume</a>
+              </li>
+              <li>
+                <a href="#sec4">Licenses and Certifications</a>
+              </li>
+              <li>
+                <a href="#sec5">Contact Form</a>
               </li>
             </ul>
           </MDBCol>
 
-          <MDBCol lg='3' md='6' className='mb-4 mb-md-0'>
-            <h5 className='text-uppercase mb-0'>Links</h5>
+          <MDBCol lg="3" md="6" className="mb-4 mb-md-0">
+            <h5 className="text-uppercase mb-0">Contact</h5>
 
-            <ul className='list-unstyled'>
+            <ul className="list-unstyled">
               <li>
-                <a href='#!' className='text-white'>
-                  Link 1
-                </a>
+                <p>
+                  <i class="fas fa-map-marker-alt pe-2"></i>Austin, Texas
+                </p>
               </li>
+              <li></li>
               <li>
-                <a href='#!' className='text-white'>
-                  Link 2
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-white'>
-                  Link 3
-                </a>
-              </li>
-              <li>
-                <a href='#!' className='text-white'>
-                  Link 4
-                </a>
+                <p>
+                  <i class="fas fa-envelope pe-2 mb-0"></i>
+                  <a href="mailto:candice.radam@gmail.com">
+                    candice.radam@gmail.com
+                  </a>
+                </p>
               </li>
             </ul>
           </MDBCol>
         </MDBRow>
       </MDBContainer>
 
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-        &copy; {new Date().getFullYear()} Copyright:{' '}
-        <a className='text-white' href='https://mdbootstrap.com/'>
-          MDBootstrap.com
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        &copy; {new Date().getFullYear()} Copyright:{" "}
+        <a
+          className="text-white"
+          href="https://github.com/zeebigbadkitty?tab=repositories"
+        >
+          Candice Radam
         </a>
       </div>
     </MDBFooter>
