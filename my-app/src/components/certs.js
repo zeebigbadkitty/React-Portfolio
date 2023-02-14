d3c83b83c33369b0ea0f223b3c3aef39e881f8ec
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-14 16:26:45 -0600

    Okay! All I need to do is get my resume to render, and these images....and I'm done.

--- my-app/src/components/certs.js
+++ my-app/src/components/certs.js
@@ -6,7 +6,11 @@ export default function Certs() {
       <div>
         <hr />
           <h2 id="sec4">Licenses and Certifications</h2>
-          The Project Management Certificate with Agile  ◽ Pathstream ◽ 2022
+          Fullstack Webdeveloper Certificate ◽ University of Texas at Austin ◽ In Progress (Expected Graduation Date: 3/2023)
+          <br></br>
+   Certified Associate in Project Management (CAPM) ◽ PMI Institute ◽  Project Management Institute (Expected Certification Date: 4/2023)
+          <br></br>
+          The Project Management Certificate with Agile  ◽ Pathstream ◽ (9/2022)
           <hr />
       </div>
     );
