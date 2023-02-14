7c1070cae44763deae7d23b8b501d2d3b6965eb2
Socially Awkward Streams <candice.radam@gmail.com>
2023-02-13 18:31:53 -0600

    Project mapping is done and placeholders are in. I'll fill in more later.

--- my-app/src/components/portfolio.js
+++ my-app/src/components/portfolio.js
@@ -1,26 +1,51 @@
 import React from "react";
 import "../css/style.css";
 
-
 export default function Portfolio() {
-  const projects=[]; // Minimum of 6 and map over components. 
-  return ( 
-<div>
-  <h2 id="sec1">Projects and Portfolio</h2>
-  <p>
-    For any questions or comments regarding the below applications, please don't hesitate to contact me. 
-  </p>
-  <div className="row">
-    <div className="col-md-4">
-      <img src="//placehold.it/300x300" className="img-responsive" />
-    </div>
-    <div className="col-md-4">
-      <img src="//placehold.it/300x300" className="img-responsive" />
-    </div>
-    <div className="col-md-4">
-      <img src="//placehold.it/300x300" className="img-responsive" /> 
-    </div>
-  </div>
+  const projects = [
+    {
+      title: "Project 1",
+      image: "//placehold.it/300x300",
+      github: "google.com",
+      deployed: "google.com",
+      description: "Blah",
+    },
+    {
+      title: "Project 2",
+      image: "//placehold.it/300x300",
+      github: "google.com",
+      deployed: "google.com",
+      description: "Blah",
+    },
+  ];
 
-  <hr /> 
-  </div> )};
+  return (
+    <div>
+      <h2 id="sec1">Projects and Portfolio</h2>
+      <p>
+        For any questions or comments regarding the below applications, please
+        don't hesitate to contact me.
+      </p>
+      <div className="row">
+        {projects.map((project) => (
+          <div className="col-md-4">
+            <div className="card">
+              <img src={project.image} className="card-img-top" alt="..." />
+              <div className="card-body">
+                <h5 className="card-title">{project.title}</h5>
+                <p className="card-text">{project.description}</p>
+                <a href={project.github} className="btn btn-primary">
+                  Github
+                </a>
+                <a href={project.deployed} className="btn btn-primary">
+                  Deployed
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
+        
+      </div>
+    </div>
+  );
+}
